453f835a483c69816170b3a4408d266dfaaa8a10
bpm2021 <polvanov.bohdan@gmail.com>
2021-07-24 23:59:06 +0300

    Фінальна версія проекту

---
+++ src/test/java/web/BaseTest.java
@@ -0,0 +1,10 @@
+package web;
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest {
+
+    protected String pathToChromeDriver = "chromedriver.exe";
+    protected WebDriver driver;
+
+}
