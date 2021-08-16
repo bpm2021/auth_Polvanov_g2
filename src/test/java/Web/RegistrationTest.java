3694f01b83393f4a515c043a2ed807343d771468
bmp <65903167+bpm2021@users.noreply.github.com>
2021-08-16 23:19:07 +0300

    Update RegistrationTest.java

--- src/test/java/Web/RegistrationTest.java
+++ src/test/java/Web/RegistrationTest.java
@@ -9,6 +9,15 @@ import DataProvider.AccountDataProviderClass;
 
 public class RegistrationTest extends BaseTest {
 
+    
+    @Epic(value = "CurrencyExchange")
+    @Story(value = "user can convert currency")
+    @Description("test convert currency amount")
+    @Issue("https://github.com/bpm2021/auth_Polvanov_g2")
+    @TmsLink(value ="PLD-567")
+    @Link(value ="link", url ="https://github.com/bpm2021/auth_Polvanov_g2")
+    @Owner(value = "Bohdan")
+    @Severity(value = SeverityLevel.CRITICAL)
     @Test(dataProvider  = "account-provider", dataProviderClass = AccountDataProviderClass.class)
     private void checkRegistrationForm(Account userAccount){
         signInPage.openWebPage(url);
